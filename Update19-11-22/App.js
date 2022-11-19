c89cd73ed66615ff2092f6b506f1f924a9cf9d9f
Irineo Contreras <72529813+Contreras24@users.noreply.github.com>
2022-11-19 12:39:28 -0600

    Create App.js

---
+++ Update19-11-22/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import Autenticacion from './screens/Autenticacion';
+import signIn from './screens/signIn';
+import home from './screens/home';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <Stack.Navigator screenOptions={{headerShown: false}}>
+      <Stack.Screen name="Autenticacion" component={Autenticacion} />
+      <Stack.Screen name="SignIn" component={signIn}/>
+      <Stack.Screen name="home" component={home}/>
+    </Stack.Navigator>
+  );
+}
+
+export default () => {
+  return (
+    <NavigationContainer>
+      <App/>
+    </NavigationContainer>
+  )
+}
